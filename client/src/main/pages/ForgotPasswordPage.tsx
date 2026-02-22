c7e450a03a5a7f4e381592f20fa50d10192c94c7
tamjidzihan <tamjidzihan@gmail.com>
2026-02-23 00:19:11 +0600

    Backend only branch

--- client/src/main/pages/ForgotPasswordPage.tsx
+++
@@ -1,9 +0,0 @@
-const ForgotPasswordPage = () => {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold">Forgot Password</h1>
-        </div>
-    )
-}
-
-export default ForgotPasswordPage
