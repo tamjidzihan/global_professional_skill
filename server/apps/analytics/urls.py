2441927388c141e278acadae0f6bafa411bcbb36
tamjidzihan <tamjidzihan@gmail.com>
2026-02-23 00:17:39 +0600

    Frontend only branch

--- server/apps/analytics/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import InstructorAnalyticsView, AdminAnalyticsView
-
-urlpatterns = [
-    path('instructor/', InstructorAnalyticsView.as_view(), name='instructor-analytics'),
-    path('admin/', AdminAnalyticsView.as_view(), name='admin-analytics'),
-]
